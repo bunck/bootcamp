2da766ea345d5cd3106b0e177a11b45ff25da7c4
David Bunck <bunck@caltech.edu>
2016-09-13 11:32:12 -0700

    no return bug fix

--- i20.py
+++ i20.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 xa_high = np.loadtxt('data/xa_high_food.csv', comments = '#')
-xa_lox = np.loadtxt('data/xa_low_food.csv', comments = '#')
+xa_low = np.loadtxt('data/xa_low_food.csv', comments = '#')
 
 def xa_to_diameter(xa):
     '''
@@ -12,3 +12,5 @@ def xa_to_diameter(xa):
 
     #A = pi * d^2 / 4
     diameter =  np.sqrt(xa *4 / np.pi)
+
+    return diameter
